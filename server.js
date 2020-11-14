6a89312bb89ffa01c0417c9e1d8dc26fe0d7f0a1
Val Castanteen <valcastanteen@Vals-Air.fios-router.home>
2020-11-14 14:13:56 -0500

    add boiler plater

---
+++ server.js
@@ -0,0 +1,30 @@
+// require our modules and initialize setting variables
+const express = require('express');
+const port = 3000;
+
+const morgan = require('morgan');
+
+
+// create the express app
+
+const app = express();
+
+// configure server 
+
+require('./config/database');
+
+app.set('view engine', 'ejs');
+
+// mount middleware
+
+app.use (morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+// TO DO mount routes
+
+//tell the application to listen
+
+app.listen(port, function() {
+    console.log('Express is listening on port:${port}'); 
+});
